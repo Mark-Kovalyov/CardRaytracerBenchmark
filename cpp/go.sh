50f95de7575ecff764c6b450925dfa4ef0746802
mayton <mark.kovalyov78@gmail.com>
2018-07-01 01:32:44 +0300

    Add CLANG support

--- cpp/go.sh
+++ cpp/go.sh
@@ -4,24 +4,41 @@ REPORT=cpp-report.csv
 
 OPTIONS="-O3"
 
+CLANG_OPTIONS="-O3"
+
 rm -f $REPORT
 
 d1="$(date +%s)"
 
-./card-raytracer-cpp /dev/null
+#./card-raytracer-cpp /dev/null
 
 d2="$(date +%s)"
 
-./card-raytracer-opt-cpp /dev/null
+#./card-raytracer-opt-cpp /dev/null
 
 d3="$(date +%s)"
 
-./card-raytracer-rwolf-cpp /dev/null
+#./card-raytracer-rwolf-cpp /dev/null
 
 d4="$(date +%s)"
+
+./card-raytracer-cpp.clang /dev/null
+
+d5="$(date +%s)"
+
+./card-raytracer-opt-cpp.clang /dev/null
+
+d6="$(date +%s)"
+
+./card-raytracer-rwolf-cpp.clang /dev/null
+
+d7="$(date +%s)"
 
 echo "Language/Type; Options ; Elapsed time" >> $REPORT
 echo "C++/Canonical ; $OPTIONS; $((d2 - d1)) s" >> $REPORT
 echo "C++/Opt       ; $OPTIONS; $((d3 - d2)) s" >> $REPORT
 echo "C++/RWolf     ; $OPTIONS; $((d4 - d3)) s" >> $REPORT
+echo "Clang++/Canonical ; $CLANG_OPTIONS; $((d5 - d4)) s" >> $REPORT
+echo "Clang++/Opt       ; $CLANG_OPTIONS; $((d6 - d5)) s" >> $REPORT
+echo "Clang++/RWolf     ; $CLANG_OPTIONS; $((d7 - d6)) s" >> $REPORT
 
--- cpp/make.sh
+++ cpp/make.sh
@@ -2,6 +2,13 @@
 
 OPTIONS="-O3"
 
+CLANG_OPTIONS="-O3"
+
 g++ $OPTIONS card-raytracer.cpp       -o card-raytracer-cpp
 g++ $OPTIONS card-raytracer-rwolf.cpp -o card-raytracer-rwolf-cpp
 g++ $OPTIONS card-raytracer-opt.cpp   -o card-raytracer-opt-cpp
+
+clang++ $CLANG_OPTIONS card-raytracer.cpp       -o card-raytracer-cpp.clang
+clang++ $CLANG_OPTIONS card-raytracer-rwolf.cpp -o card-raytracer-rwolf-cpp.clang
+clang++ $CLANG_OPTIONS card-raytracer-opt.cpp   -o card-raytracer-opt-cpp.clang
+
