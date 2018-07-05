f0a2f2bc62671034da724b1bf63e713448b14f48
vzozulya <vzozulja@mail.ru>
2018-07-05 10:55:00 +0300

    optimize for time, AVX2 version is faster

--- c/make.cmd
+++ c/make.cmd
@@ -2,3 +2,4 @@ gcc Raytracer_handofdos_r2.c -O3 -march=native -m64 -msse4.2 -ffast-math -o Rayt
 gcc Raytracer_handofdos_r2.c -O3 -march=native -m64 -msse4.2 -ffast-math -DAVX_VERSION -o Raytracer_handofdosavx2.exe
 ::gcc Raytracer_handofdos.c -O0 -march=native -m64 -msse4.2 -ffast-math -DAVX_VERSION -o Raytracer_handofdosAVX.exe   -pg
 gcc Raytracer_handofdos_r3experimental.c -O3 -march=native -m64 -msse4.2 -ffast-math -DAVX_VERSION -o Raytracer_handofdosAVX3.exe   
+gcc Raytracer_handofdos_r3experimental.c -O3 -march=native -m64 -msse4.2 -ffast-math -DAVX_VERSION -fopenmp -o Raytracer_handofdosAVX3mp.exe   
