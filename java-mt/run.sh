60eab17c203cdfb5dd4ba205e1562352b2ea963f
mayton <mark.kovalyov78@gmail.com>
2019-02-20 00:21:29 +0200

    Improved run script

--- java-mt/run.sh
+++ java-mt/run.sh
@@ -2,9 +2,6 @@
 
 java -jar target/CardRaytracerMt-1.0.jar -h
 
-
-# 2 Threads
-
 time java \
  -server \
  -XX:CompileThreshold=2 \
@@ -20,53 +17,32 @@ time java \
  -server \
  -XX:CompileThreshold=2 \
  -jar target/CardRaytracerMt-1.0.jar \
-   --parallelism 2 \
+   --parallelism 3 \
    --segperf     G_RATIO \
    --segstr      AD \
-   --segmentsize 1024 \
+   --segmentsize 2048 \
    --drawseg \
-   --filename    out-g-ratio-1024px-2t.bmp
-
-exit
-
-# 3 Threads
+   --filename    out-g-ratio-2048px-3t.bmp
 
 time java \
  -server \
  -XX:CompileThreshold=2 \
  -jar target/CardRaytracerMt-1.0.jar \
-  3 G_RATIO AD 2048 out-g-ratio-2048px-3t.bmp drawseg
+   --parallelism 4 \
+   --segperf     G_RATIO \
+   --segstr      AD \
+   --segmentsize 2048 \
+   --drawseg \
+   --filename    out-g-ratio-2048px-4t.bmp
 
 time java \
  -server \
  -XX:CompileThreshold=2 \
  -jar target/CardRaytracerMt-1.0.jar \
-  3 G_RATIO AD 1024 out-g-ratio-1024px-3t.bmp drawseg
+   --parallelism 5 \
+   --segperf     G_RATIO \
+   --segstr      AD \
+   --segmentsize 2048 \
+   --drawseg \
+   --filename    out-g-ratio-2048px-5t.bmp
 
-# 4 Threads
-
-time java \
- -server \
- -XX:CompileThreshold=2 \
- -jar target/CardRaytracerMt-1.0.jar \
-  4 G_RATIO AD 2048 out-g-ratio-2048px-4t.bmp drawseg
-
-time java \
- -server \
- -XX:CompileThreshold=2 \
- -jar target/CardRaytracerMt-1.0.jar \
-  4 G_RATIO AD 1024 out-g-ratio-1024px-4t.bmp drawseg
-
-# 5 Threads
-
-time java \
- -server \
- -XX:CompileThreshold=2 \
- -jar target/CardRaytracerMt-1.0.jar \
-  5 G_RATIO AD 2048 out-g-ratio-2048px-5t.bmp drawseg
-
-time java \
- -server \
- -XX:CompileThreshold=2 \
- -jar target/CardRaytracerMt-1.0.jar \
-  5 G_RATIO AD 1024 out-g-ratio-1024px-5t.bmp drawseg
