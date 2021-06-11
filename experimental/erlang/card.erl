8d870629b9972f1b04e2b585e9c8bda03fe78ca1
Mark.Kovalyov <mark.kovalyov78@gmail.com>
2021-06-11 21:42:18 +0300

    Changes in Erlang

--- experimental/erlang/card.erl
+++ experimental/erlang/card.erl
@@ -7,6 +7,7 @@
 % 07-May-2018 - (mayton) In beginning...
 % 07-Sep-2019 - continue
 % 12-Mar-2021 - Add recursive route
+% 13-Mar-2021 - Add sampler, tracer
 
 
 -module(card).
@@ -16,10 +17,6 @@
 %-include_lib("stdlib/src/random.hrl").
 
 -export([main/0]).
--export([newvec/3]).
--export([sprod/2]).
--export([sum/2]).
--export([pixelRoute/2]).
 
 -define(WIDTH, 5).
 -define(HEIGHT, 7).
@@ -27,6 +24,20 @@
 -record(vector, {x :: double,
                  y :: double,
                  z :: double}).
+
+g_data() ->
+  [
+            2#00111100011100010010,
+            2#01000100100000010100,
+            2#01000100100000011000,
+            2#00111100111110010100,
+            2#00000100100010010010,
+            2#00000100100010010001,
+            2#00111000011100010000,
+            2#00000000000000010000,
+            2#00000000000000010000
+].
+
 
 newvec(X,Y,Z) -> #vector{x=X,y=Y,z=Z}.
 
@@ -44,34 +55,46 @@ norm(A) -> prod(A, 1.0 / math:sqrt(sprod(A,A))).
 
 % TODO: Should be completed
 
-sampler() -> 1.
-
 tracer() -> 1.
 
 %subPixelRoute(X, Y, R) ->
 %  T = #vector {X = a * (random:uniform() - 0.5) * 99 + b * (Random() - .5) * 99}
   
 
+
+sampler(Ovect, Dvect) -> 
+  Mint = tracer(),
+  ok.
+
+
+rayJitter(Avec, Bvec, Rcount) when Rcount > 0 ->
+  RandomVal = random:uniform(),
+  % Vector t = 
+  %       a * (random() - 0.5) * 99.0 + 
+  %       b * (random() - 0.5) * 99.0
+  Tvec = sum(99.0 * prod(Avec, random:uniform() - 0.5),
+             99.0 * prod(Bvec, random:uniform() - 0.5)),
+  % p = sampler(Vector(17, 16, 8) + t, 
+  %             !(t * -1 + (a * (Random() + x) + b * (y + Random()) + c) * 16)) * 3.5 + p;
+  Ovec = sum(newvec(17.0,16.0,8.0),Tvec)
+  Dvec = newvec(0.0,0.0,0.0),
+  Pvec = sampler(Ovec, Dvec),
+  rayJitter(Avec, Bvec, Rcount - 1).
+
+
 pixelRoute(X, Y) when X < ?WIDTH , Y < ?HEIGHT -> 
-  %Vector g = !Vector(-6, -16, 0);
-  %Vector a = !(Vector(0, 0, 1) ^ g) * .002;
-  %Vector b = !(g ^ a) * .002;
-  %Vector c = (a + b) * -256 + g;
-
-  G = norm(newvec(-6.0,-16.0,0.0)),
-  A = prod(norm(newvec(0.0,0.0,1.0)),0.002),
-  B = norm(prod(vprod(G,A),0.002)),
-  C = sum((prod(sum(A,B),-256.0)),G),
-
-  F = random:uniform(),
-  io:fwrite("(~p,~p,~p);~n",[X,Y,F]),
+  Gvec = norm(newvec(-6.0,-16.0,0.0)),
+  Avec = prod(norm(newvec(0.0,0.0,1.0)),0.002),
+  Bvec = norm(prod(vprod(Gvec,Avec),0.002)),
+  Cvec = sum((prod(sum(Avec,Bvec),-256.0)),Gvec),
+  %io:fwrite("(~p,~p,~p);~n",[X,Y,F]),
+  rayJitter(Avec,Bvec, 64),
   pixelRoute(X + 1, Y);
 
 pixelRoute(X, Y) when X == ?WIDTH , Y < ?HEIGHT -> 
   pixelRoute(0, Y + 1);
 
 pixelRoute(X, Y) -> ok.
-
 
 main() -> 
    io:fwrite("P6 ~p ~p 255 ",[?WIDTH,?HEIGHT]),
